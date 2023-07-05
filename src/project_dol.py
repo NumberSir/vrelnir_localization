5e3bd7d6317974b0464802b60214e917e8611a40
Number_Sir <number_sir@126.com>
2023-07-06 01:56:54 +0800

    Update project_dol.py

--- src/project_dol.py
+++ src/project_dol.py
@@ -541,7 +541,7 @@ class ProjectDOL:
             game_html = self.game_dir / file
             if file.startswith("Degrees of Lewdity") and file.endswith("html"):
                 dol_html = "beta" if GITHUB_ACTION_ISBETA else "index"
-                game_html = self.nomalized_path(f"{game_dir_path}\\{file}")
+                game_html = self.game_dir / file
                 shutil.copyfile(
                     game_html,
                     dol_chinese_path / f"{dol_html}.html",
@@ -561,11 +561,14 @@ class ProjectDOL:
                 )
         dol_chinese_img_path = dol_chinese_path / "img"
 
+        def ignorefile(dir, files):
+            return [f for f in files if f.endswith(".js") or f.endswith(".bat")]
+
         shutil.copytree(
             self.game_dir / "img",
             dol_chinese_img_path,
             True,
-            ignore=lambda files: [f for f in files if f.endswith(".js") or f.endswith(".bat")],
+            ignore=ignorefile,
             dirs_exist_ok=True,
         )
         logger.info("##### 复制到 git 已完毕! ")
