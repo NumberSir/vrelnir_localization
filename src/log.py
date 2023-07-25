97c9b9ad4fd767346fbb0ca64b7bb70625f5adda
深渊の鸽子 <ghzero97@gmail.com>
2023-07-25 01:19:29 +0100

    [update] fix logger color support

--- src/log.py
+++ src/log.py
@@ -4,6 +4,6 @@ import sys
 logger_.remove()
 logger_.add(sys.stdout, format="<g>{time:HH:mm:ss}</g> | [<lvl>{level}</lvl>] | {message}", colorize=True)
 
-logger = logger_.opt(colors=True)
+logger = logger_
 
 __all__ = ["logger"]
