abdccdc187b3e32e3a428ab3ef7be2ed954f7d39
cphxj123 <135980455+cphxj123@users.noreply.github.com>
2023-08-29 14:05:16 +0800

    Update project_dol.py

--- src/project_dol.py
+++ src/project_dol.py
@@ -448,17 +448,25 @@ class ProjectDOL:
             fp.writelines(lines)
 
     def change_version(self, version: str = ""):
-        """字体间距"""
+        """修改版本号"""
         if self._type == "world":
-            FILE_VERSION_EDIT_COMMON = FILE_VERSION_EDIT_WORLD
-        with open(FILE_VERSION_EDIT_COMMON, "r", encoding="utf-8") as fp:
-            lines = fp.readlines()
-        for idx, line in enumerate(lines):
-            if "version: " in line.strip():
-                lines[idx] = f'version: "{version}",\n'
-                break
-        with open(FILE_VERSION_EDIT_COMMON, "w", encoding="utf-8") as fp:
-            fp.writelines(lines)
+            with open(FILE_VERSION_EDIT_WORLD, "r", encoding="utf-8") as fp:
+                lines = fp.readlines()
+            for idx, line in enumerate(lines):
+                if "version: " in line.strip():
+                    lines[idx] = f'version: "{version}",\n'
+                    break
+            with open(FILE_VERSION_EDIT_WORLD, "w", encoding="utf-8") as fp:
+                fp.writelines(lines)
+        else:
+            with open(FILE_VERSION_EDIT_COMMON, "r", encoding="utf-8") as fp:
+                lines = fp.readlines()
+            for idx, line in enumerate(lines):
+                if "version: " in line.strip():
+                    lines[idx] = f'version: "{version}",\n'
+                    break
+            with open(FILE_VERSION_EDIT_COMMON, "w", encoding="utf-8") as fp:
+                fp.writelines(lines)
 
     @staticmethod
     def _is_lack_angle(line_zh: str, line_en: str):
